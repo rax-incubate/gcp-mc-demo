54c62ab1440cebf063b47fc3230230f185b14211
srirajan <sriram.rajan@gmail.com>
2023-08-21 09:58:07 +0100

    added css to the html app

--- apps/examples/myapp/Readme.md
+++ apps/examples/myapp/Readme.md
@@ -1,7 +1,2 @@
 # Build and push the image
-
-MYAPP_REPO=us-east1-docker.pkg.dev/gcp-mc-demo-prime/myapp/myapp:1.10
-
-docker build .  -t ${MYAPP_REPO}
-
-docker push ${MYAPP_REPO}
+MYAPP_REPO=us-east1-docker.pkg.dev/gcp-mc-demo-prime/myapp/myapp:1.20  && docker build .  -t ${MYAPP_REPO} && docker push ${MYAPP_REPO}
--- apps/examples/myapp/main.py
+++ apps/examples/myapp/main.py
@@ -1,14 +1,12 @@
 import os
 import psycopg2
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder="static/templates")
 
 @app.route("/run")
 
 def hello_world():
-
     # All these variables are read from the Kubernetes secrets set on the same namespace as the app
     db_host = os.environ.get("DB_HOST", "127.0.0.1")
     db_host_port = os.environ.get("DB_HOST_PORT", "5432")
@@ -18,31 +16,27 @@ def hello_world():
     db_pwd = os.environ.get("DB_PWD", "imdb")
 
     try:
-        conn = psycopg2.connect(
-            database=db_name, user=db_user, password=db_pwd, host=db_host, port= db_host_port
-            )
+        conn = psycopg2.connect(database=db_name, user=db_user, password=db_pwd, host=db_host, port=db_host_port)
     except OperationalError as err:
         print(err)
 
     try:
         cursor = conn.cursor()
-        cursor.execute("SELECT * from " + db_table  + " LIMIT 100")
+        cursor.execute("select primary_title,start_year from " + db_table + " where title_type='movie' and genres='Comedy' and start_year > '2010'  limit 100")
     except Exception as err:
         print(err)
     rows = cursor.fetchall()
     conn.commit()
     conn.close()
-    
     env = os.environ.get("ENV", "default")
-    html = env + "<br>\n"
-    
+    my_env = "Environment:" + env
+
+    gen_html = ""
     for row in rows:
-        html = html + ','.join(map(str, row))
-        html = html + "<br>\n"
-    
-    return(html)
+        gen_html = gen_html + "<tr><td>" + row[0] + "</td><td>" + str(row[1]) + "</td></tr>" + "\n"
+
+    return render_template("list.html", env=my_env, content=gen_html)
 
 if __name__ == "__main__":
     listen_port = os.environ.get("CONTAINER_PORT", "8080")
     app.run(debug=True, host="0.0.0.0", port=listen_port)
-
